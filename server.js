101a2ad0d4f07ebe9e51c9ac4590ea81d4770d84
thiago <leceido@gmail.com>
2023-09-01 13:51:24 -0300

    Ajustes server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http')
 const app = require('./app')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT? Number(process.env.PORT): 3000
 const HOST = "0.0.0.0";
 const server = http.createServer(app)
 
 
 server.listen(PORT, HOST, () => {
-    console.log(`Servidor rodando! http://localhost:${PORT}/`)
+    console.log(`Servidor rodando!`)
 })
